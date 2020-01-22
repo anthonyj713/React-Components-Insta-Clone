7a36788cf5550e4a3d35a24f0b02ebd1e5e97c8e
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-01-22 09:52:06 -0700

    Just getting the project started

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import "./components/PostsContainer/PostsPage";
+import "./components/SearchBar/SearchBarContainer";
+import dummyData from "./dummy-data" 
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 
 const App = () => {
+  const [data] = useState(dummyData);
   // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
