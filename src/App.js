070d0db69b97ce5be9d6cfaf42687726d7fccc88
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-01-22 13:56:56 -0700

    Search Bar complete.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ Look at each file to see where you need to pass props or add code
 import React, {useState} from "react";
 import "./App.css";
 import PostsPage from "./components/PostsContainer/PostsPage";
-import "./components/SearchBar/SearchBarContainer";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
 import dummyData from "./dummy-data" 
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
@@ -17,6 +17,7 @@ const App = () => {
   // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
+      <SearchBar/> 
       <PostsPage posts = {data}/>
       {/* Add components here  and pass props where appropriate */}
     </div>
